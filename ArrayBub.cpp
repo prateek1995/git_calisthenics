dfc0134e55b7185076cc100ca6fb97d1ffe308df
Pratheek <apratheekreddy@gmail.com>
2017-03-09 23:42:58 +0530

    changed the elements order

--- ArrayBub.cpp
+++ ArrayBub.cpp
@@ -47,16 +47,16 @@ int main()
     ArrayBub arr(100);
 
 
-    arr.insert(22);
-    arr.insert(88);
-    arr.insert(44);
-    arr.insert(99);
-    arr.insert(11);
     arr.insert(33);
-    arr.insert(0);
-    arr.insert(55);
-    arr.insert(66);
     arr.insert(77);
+    arr.insert(11);
+    arr.insert(55);
+    arr.insert(22);
+    arr.insert(0);
+    arr.insert(44);
+    arr.insert(66);
+    arr.insert(99);
+    arr.insert(88);
 
     arr.display();
 
